5fee42f805ac002bc43795c40372c02ab4890dd9
conor-brennan <conor.a.brennan@gmail.com>
2020-04-10 14:01:23 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PasswordGenerator
+ A quick program to generate passwords that I wrote after someone in Russia logged into my Spotify.
---
+++ src/PasswordGenerator.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+public class PasswordGenerator {
+    // character set
+    private String[] characters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
+            "x","y","z","1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","^","&","*","?","-",
+            "_","+","=","/",",",".","{","}","[","]","<",">"};
+    // size of character set
+    private int size = characters.length;
+    // length of password you'd like
+    private int length = 20;
+    private Random random;
+
+    // randomizes upper/lower case for letters
+    private void caseRandomizer(int r) {
+        // 0 = lower case, 1 = upper case
+        int c = random.nextInt(2);
+        String l = characters[r];
+
+        if (c == 1) {
+            System.out.print(l.toUpperCase());
+        } else {
+            System.out.print(l);
+        }
+    }
+
+    private void generate() {
+        for (int i = 0; i < length; i++) {
+            int r = random.nextInt(size);
+            //check for letter
+            if (r < 26) {
+                caseRandomizer(r);
+            } else {
+                System.out.print(characters[r]);
+            }
+        }
+        System.out.println("");
+    }
+
+
+    public PasswordGenerator() {
+        random = new Random();
+        for (int i = 0; i < 100; i++) {
+            generate();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new PasswordGenerator();
+    }
+}
